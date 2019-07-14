78221d56d36a052f043bce4cce7bb2690f55c211
oogiepanda <harlan.oogie@gmail.com>
2019-07-13 18:32:07 -0700

    searches now save upon restarting the app

---
+++ PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/PollService.java
@@ -0,0 +1,63 @@
+package com.bignerdranch.android.photogallery;
+
+import android.app.IntentService;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.util.Log;
+
+import java.util.List;
+
+public class PollService extends IntentService {
+    private static final String TAG = "PollService";
+
+    public static Intent newIntent(Context context) {
+        return new Intent(context, PollService.class);
+    }
+
+    public PollService() {
+        super(TAG);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        if (isNetworkAvailableAndConnected()) {
+            return;
+        }
+        String query = QueryPreferences.getStoredQuery(this);
+        String lastResultId = QueryPreferences.getLastResultId(this);
+        List<GalleryItem> items;
+
+        if (query ==null) {
+            items = new FlickrFetchr().fetchRecentPhotos();
+        }
+        else {
+            items = new FlickrFetchr().searchPhotos(query);
+        }
+
+        if (items.size() == 0) {
+            return;
+        }
+
+        String resultId = items.get(0).getId();
+        if (resultId.equals(lastResultId)) {
+            Log.i(TAG, "Got an old result: " + resultId);
+        }
+        else {
+            Log.i(TAG, "Got new result: " + resultId);
+        }
+
+        QueryPreferences.setLastResultId(this, resultId);
+    }
+
+    private boolean isNetworkAvailableAndConnected() {
+        ConnectivityManager cm =
+                (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+
+        boolean isNetworkAvailable = cm.getActiveNetworkInfo() != null;
+        boolean isNetworkConnected = isNetworkAvailable &&
+                cm.getActiveNetworkInfo().isConnected();
+
+        return isNetworkConnected;
+    }
+}
--- PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/QueryPreferences.java
+++ PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/QueryPreferences.java
@@ -1,10 +1,12 @@
 package com.bignerdranch.android.photogallery;
 
 import android.content.Context;
+import android.net.ConnectivityManager;
 import android.preference.PreferenceManager;
 
 public class QueryPreferences {
     private static final String PREF_SEARCH_QUERY = "searchQuery";
+    private static final String PREF_LAST_RESULT_ID = "lastResultId";
 
     public static String getStoredQuery(Context context) {
         return PreferenceManager.getDefaultSharedPreferences(context)
@@ -15,6 +17,18 @@ public class QueryPreferences {
         PreferenceManager.getDefaultSharedPreferences(context)
                 .edit()
                 .putString(PREF_SEARCH_QUERY, query)
+                .apply();
+    }
+
+    public static String getLastResultId(Context context) {
+        return PreferenceManager.getDefaultSharedPreferences(context)
+                .getString(PREF_LAST_RESULT_ID, null);
+    }
+
+    public static void setLastResultId(Context context, String lastResultId) {
+        PreferenceManager.getDefaultSharedPreferences(context)
+                .edit()
+                .putString(PREF_LAST_RESULT_ID, lastResultId)
                 .apply();
     }
 }
